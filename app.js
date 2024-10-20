9f2750532f9e53c3249338ab459a54822fc83c47
NehaPhadtare1607 <99655483+NehaPhadtare1607@users.noreply.github.com>
2024-10-20 13:31:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const openBtn=document.querySelector('.open-btn');
+const closeBtn=document.querySelector('.close-btn');
+const wrap=document.querySelectorAll('.wrap');
+
+
+openBtn.addEventListener("click",function(){
+    for(let i=0;i<wrap.length;i++){
+        wrap[i].classList.add('visible');
+
+    }
+})
+
+closeBtn.addEventListener("click",function(){
+    for(let i=0;i<wrap.length;i++){
+        wrap[i].classList.remove('visible');
+
+    }
+})
